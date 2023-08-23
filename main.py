95360459412f0ada84e47addb493330f54dbb798
Brian Gichungu <el20bwg@leeds.ac.uk>
2023-08-23 22:34:37 +0100

    Added select_post function

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ def get_posts():
     if response.status_code == 200:
         return response.json()
     return []
+
+
+def select_post(post_id):
+    response = requests.get(f"{API_URL}/posts/{post_id}")
+    if response.status_code == 200:
+        return response.json()
+    return "Post Id does not exist"
