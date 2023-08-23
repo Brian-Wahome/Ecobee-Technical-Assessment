383ae243458520e6d7ac5986b01698830c0c4c44
Brian Gichungu <el20bwg@leeds.ac.uk>
2023-08-23 21:41:39 +0100

    Removed random selection of ten posts as it results into errors while testing.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import random
 
 API_URL = "https://jsonplaceholder.typicode.com"
 
@@ -7,5 +6,5 @@ API_URL = "https://jsonplaceholder.typicode.com"
 def get_posts():
     response = requests.get(f"{API_URL}/posts")
     if response.status_code == 200:
-        return random.sample(response.json(), 10)
+        return response.json()
     return []
