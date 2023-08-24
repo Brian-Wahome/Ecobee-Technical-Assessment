88c5cea60c1ec838fecf68fdc40d3bbae4316d9c
Brian Gichungu <el20bwg@leeds.ac.uk>
2023-08-24 14:34:20 +0100

    Added function to search for posts

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def view_post_comments(post_id):
 def post_comment(post_id, name, email, body):
     comment_id = 500
     comment = {
-        "id": comment_id+1,
+        "id": comment_id + 1,
         "name": name,
         "email": email,
         "body": body
@@ -35,3 +35,14 @@ def post_comment(post_id, name, email, body):
     response = requests.post(f"{API_URL}/posts/{post_id}/comments", json=comment)
 
     return response.status_code == 201
+
+
+def search_posts_by_title(title):
+    posts = get_posts()
+    matching_posts = []
+    for post in posts:
+        if title.lower() in post['title'].lower():
+            matching_posts.append(post)
+    return matching_posts
+
+
